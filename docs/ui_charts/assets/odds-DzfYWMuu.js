4208a5f962eb2828e0dd0dfa6a4189501773dff9
seb <seb@email>
2024-08-27 17:23:15 +0100

    .

--- docs/ui_charts/assets/odds-DzfYWMuu.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as t}from"./client-B8KsQLkO.js";import o from"https://cdn.jsdelivr.net/npm/react@18.3.1/+esm";import{M as r}from"./index-AY2wESIt.js";import"https://cdn.jsdelivr.net/npm/react-dom@18.3.1/+esm";e.createRoot(document.getElementById("root")).render(t.jsx(o.StrictMode,{children:t.jsx(r,{})}));
