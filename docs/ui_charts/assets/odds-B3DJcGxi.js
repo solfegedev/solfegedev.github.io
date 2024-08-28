05d2832b248e7af99f376912ebc15c553bb8e989
seb <seb@email>
2024-08-28 11:14:41 +0100

    .

--- docs/ui_charts/assets/odds-B3DJcGxi.js
+++
@@ -1,1 +0,0 @@
-import{c as o,j as t}from"./client-B8KsQLkO.js";import r from"https://cdn.jsdelivr.net/npm/react@18.3.1/+esm";import{M as e}from"./Main-BpTrCD9o.js";/* empty css              */import"https://cdn.jsdelivr.net/npm/react-dom@18.3.1/+esm";o.createRoot(document.getElementById("root")).render(t.jsx(r.StrictMode,{children:t.jsx(e,{})}));
