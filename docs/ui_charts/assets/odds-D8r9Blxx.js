c2d4c2e939ddfa7fa34eb72fd22244c5db0cda6c
seb <seb@email>
2024-08-29 18:32:29 +0100

    .

--- docs/ui_charts/assets/odds-D8r9Blxx.js
+++
@@ -1,1 +0,0 @@
-import{c as o,j as t}from"./client-B8KsQLkO.js";import r from"https://cdn.jsdelivr.net/npm/react@18.3.1/+esm";import{M as e}from"./Main-CjUQbf-p.js";import"./index-CRJBsK6O.js";import"https://cdn.jsdelivr.net/npm/react-dom@18.3.1/+esm";o.createRoot(document.getElementById("root")).render(t.jsx(r.StrictMode,{children:t.jsx(e,{})}));
