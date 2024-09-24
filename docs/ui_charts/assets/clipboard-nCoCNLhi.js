5953c6a0bec773ce7457f15ee36e6bf93b1070ae
seb <seb@email>
2024-09-25 00:40:20 +0100

    .

---
+++ docs/ui_charts/assets/clipboard-nCoCNLhi.js
@@ -0,0 +1,1 @@
+const e=o=>{navigator.clipboard.writeText(o).then(()=>{console.warn("Successfully copied the text")}).catch(()=>{console.warn("Something went wrong while copying")})};export{e as c};
