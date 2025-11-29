2f220d38c090a0425d340585e52e35b944c6f5e4
seb <seb@email>
2025-11-29 14:12:41 +0000

    CI automated commit

--- docs/assets/api-C-gcI8S5.js
+++
@@ -1,1 +0,0 @@
-const e=()=>"https://api8016.solfegeai.com",t=()=>e().replace(/\/+$/,"");export{t as g};
---
+++ docs/assets/api-CH_Pe6Vg.js
@@ -0,0 +1,1 @@
+const e=()=>"https://api8016.solfegeai.com",s=()=>e().replace(/\/+$/,""),a=s();export{a as A};
