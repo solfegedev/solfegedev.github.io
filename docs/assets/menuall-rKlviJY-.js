60495e302ccc5c782efc60b58e0def9f62e8506f
seb <seb@email>
2026-02-10 01:44:01 +0000

    CI automated commit

--- docs/assets/menuall-rKlviJY-.js
+++
@@ -1,1 +0,0 @@
-import"./index-F_Pwjxo7.js";import{j as o}from"./jsx-runtime-u17CrQMm.js";import{A as e}from"./app-Bob3athT.js";const t=document.getElementById("root");if(!t)throw new Error("Root element #root not found");document.body.classList.add("menuall-page");ReactDOM.createRoot(t).render(o.jsx(React.StrictMode,{children:o.jsx(e,{})}));
