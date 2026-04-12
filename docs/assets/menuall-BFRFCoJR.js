111ff3a9cede1456b3c9ca1d8cba9833664beb3a
seb <seb@email>
2026-04-12 14:26:08 +0100

    CI automated commit

---
+++ docs/assets/menuall-BFRFCoJR.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-DIP-oa02.js";import"./pages-Bo2UA9Rd.js";import{t}from"./app-B0Svjp41.js";var n=e(),r=document.getElementById(`root`);if(!r)throw Error(`Root element #root not found`);document.body.classList.add(`menuall-page`),ReactDOM.createRoot(r).render((0,n.jsx)(React.StrictMode,{children:(0,n.jsx)(t,{})}));
