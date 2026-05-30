009a63db1236084eb6e4f83b54824f07138e2127
seb <seb@email>
2026-05-30 01:35:28 +0100

    CI automated commit

---
+++ docs/_astro/api.Bg-vbRZu.js
@@ -0,0 +1,1 @@
+const e=()=>"https://api8016.solfegeai.com".replace(/\/+$/,""),r=e();export{r as A};
---
+++ docs/_astro/index.BPvUYDC2.js
@@ -0,0 +1,1 @@
+import{g as r}from"./index.CmUPVzYF.js";import{r as o}from"./index.JWvMWs-t.js";var t=o();const m=r(t);export{m as R,t as r};
